bb179463ff844f264a9498f2815c9733132c90f2
Alter-Zero <rafaelhibiki@gmail.com>
2020-03-31 10:26:59 -0300

    Added readme and comments

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Password generator
+
+An app that generates a random password based on the options chosen by the user. With letters, uper case letters, symbols and numbers.
+
+You cen see it running [here](https://alter-zero.github.io/PasswordGenerator/)
--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const form = document.getElementById("passwordGeneratorForm")
 const passwordDisplay = document.getElementById("passwordDisplay")
 
 
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
+characterAmountRange.addEventListener("input", syncCharacterAmount)
+
+//Sync the characters on the screen to have the same value which was chosen by the user
+function syncCharacterAmount(e){
+    const value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value
+}
+
+
+//Define the password values
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -16,11 +28,16 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
         arrayFromLowToHigh(91,96)
     ).concat(arrayFromLowToHigh(123,126))
 
+//Generate a random value of the password
+function arrayFromLowToHigh(low, high){
+    const array = []
+    for(let i = low; i <= high; i++){
+        array.push(i)
+    }
+    return array
+}
 
-characterAmountNumber.addEventListener("input", syncCharacterAmount)
-characterAmountRange.addEventListener("input", syncCharacterAmount)
-
-
+//Define the configurarions for the generatePassword and show on the screen
 form.addEventListener('submit', e =>{
     e.preventDefault()
     const characterAmount = characterAmountNumber.value
@@ -32,6 +49,7 @@ form.addEventListener('submit', e =>{
     passwordDisplay.innerText = password
 })
 
+//Get the values of the form and generates the random password
 function generatePassword(characterAmount, includeUppercase,
     includeNumbers, includeSymbols){
     let charCodes = LOWERCASE_CHAR_CODES
@@ -44,18 +62,4 @@ function generatePassword(characterAmount, includeUppercase,
         passwordCharacters.push(String.fromCharCode(characterCode))
     }
     return passwordCharacters.join("")
-}
-
-function arrayFromLowToHigh(low, high){
-    const array = []
-    for(let i = low; i <= high; i++){
-        array.push(i)
-    }
-    return array
-}
-
-function syncCharacterAmount(e){
-    const value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
 }
